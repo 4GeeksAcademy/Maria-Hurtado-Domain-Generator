4a02baf1f3254f89676ece76be828f6e1150127e
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2023-11-16 16:36:55 +0000

    Adding a buttom to generate the list of domains

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn-domain").addEventListener("click", () => {
+    const domainsList = generateDomains();
+    const domainsListHTML = domainsList
+      .map(domainName => `<li>${domainName}</li>`)
+      .join("");
+
+    document.querySelector(
+      "#domains-generated"
+    ).innerHTML = `<ul>${domainsListHTML}</ul>`;
+  });
 };
+
+function generateDomains() {
+  let pronouns = ["the", "my", "your"];
+  let adjectives = ["big", "awesome", ""];
+  let nouns = ["rodeo", "club", "restaurant"];
+  let domains = [".com", ".net", ".es"];
+  let domainsGenerated = [];
+
+  for (let pronoun = 0; pronoun < pronouns.length; pronoun++) {
+    for (let adjective = 0; adjective < adjectives.length; adjective++) {
+      for (let noun = 0; noun < nouns.length; noun++) {
+        for (let domain = 0; domain < domains.length; domain++) {
+          let domainName =
+            pronouns[pronoun] +
+            adjectives[adjective] +
+            nouns[noun] +
+            domains[domain];
+          domainsGenerated.push(domainName);
+        }
+      }
+    }
+  }
+  return domainsGenerated;
+}
